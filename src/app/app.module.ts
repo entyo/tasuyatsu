e8e0c111bdaa596918d4244fbcbf0b9942520d00
e_ntyo <entyo.contact@gmail.com>
2017-05-26 13:07:37 +0900

    :sparkles: ファイルアップロードができるようにした

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,16 @@ import { PlayerComponent } from './player/player.component';
 
 import { Ng2BootstrapModule } from 'ng2-bootstrap';
 
+import { FileUploaderComponent } from './file-uploader/file-uploader.component';
+import { FileSelectDirective, FileDropDirective } from 'ng2-file-upload';
+
 @NgModule({
   declarations: [
     AppComponent,
-    PlayerComponent
+    PlayerComponent,
+    FileUploaderComponent,
+    FileSelectDirective,
+    FileDropDirective
   ],
   imports: [
     BrowserModule,
