3aca8acbd4c32e1216c5ebc7ace911b3f53fa653
e_ntyo <entyo.contact@gmail.com>
2017-05-28 23:09:30 +0900

    :recycle: ファイルアップロードしたらバックエンドのAPI必要だしやめた

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,9 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { Ng2BootstrapModule } from 'ng2-bootstrap';
-import { FileSelectDirective, FileDropDirective } from 'ng2-file-upload';
 
 import { AppComponent } from './app.component';
 import { PlayerComponent } from './player/player.component';
-import { FileUploaderComponent } from './file-uploader/file-uploader.component';
 import { PlayerListComponent } from './player-list/player-list.component';
 
 import { SharedModule } from './shared/shared.module';
@@ -19,9 +17,6 @@ import { AppStore } from './app.store';
   declarations: [
     AppComponent,
     PlayerComponent,
-    FileUploaderComponent,
-    FileSelectDirective,
-    FileDropDirective,
     PlayerListComponent
   ],
   imports: [
