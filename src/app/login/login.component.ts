c0e938f416e15ee5847284aaaefb397a8915e5e6
e_ntyo <entyo.contact@gmail.com>
2017-06-11 23:13:24 +0900

    :bug: ログイン・ログアウトがビューに即時反映されなかったのを修正

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,35 +13,17 @@ export class LoginComponent {
   allowedAction: string;
 
   constructor(public afAuth: AngularFireAuth) {
-     this.isLoggedIn = firebase.auth().currentUser ? true : false;
-     this.allowedAction = this.isLoggedIn ? "ログアウト" : "ログイン"
-  }
-
-  login() {
-    this.afAuth.auth.signInWithPopup(new firebase.auth.TwitterAuthProvider())
-    .then(() => {
-      this.isLoggedIn = true;
-      this.allowedAction = "ログアウト";
-    }, () => {
-      console.log("login failed...");
-    });
-  }
-
-  logout() {
-    this.afAuth.auth.signOut()
-    .then(() => {
-      this.isLoggedIn =  false;
-      this.allowedAction = "ログイン";
-    }, () => {
-      console.log("logout failed...");
+    afAuth.authState.subscribe(auth => {
+      this.isLoggedIn = auth ? true : false;
+      this.allowedAction = this.isLoggedIn ? "ログアウト" : "ログイン"
     });
   }
 
   switchLoginStatus() {
     if (this.isLoggedIn) {
-      this.logout();
+      this.afAuth.auth.signOut();
     } else {
-      this.login();
+      this.afAuth.auth.signInWithPopup(new firebase.auth.TwitterAuthProvider());
     }
   }
   
